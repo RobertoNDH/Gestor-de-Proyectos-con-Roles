aa50a7cdf8c9cd89fabdd03324a2e45714540aff
RobertoNDH <robertonauzet@gmail.com>
2026-05-03 22:59:31 +0100

    feat: registrar modelos en el panel de administración de Django

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Role, Project, Assignment, Task, Message
 
-# Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Role)
+admin.site.register(Project)
+admin.site.register(Assignment)
+admin.site.register(Task)
+admin.site.register(Message)
