8cb773a063057e6f51c12c03ffce5c57d8b579b5
RobertoNDH <robertonauzet@gmail.com>
2026-05-04 22:51:00 +0100

    feat: implementar vistas principales y formulario de tareas

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from django import forms
+from .models import Task
 
 User = get_user_model()
 
@@ -7,3 +9,17 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'assigned_to', 'status', 'due_date']
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+        
+    def __init__(self, *args, **kwargs):
+        project = kwargs.pop('project', None)
+        super().__init__(*args, **kwargs)
+        if project:
+            self.fields['assigned_to'].queryset = User.objects.filter(assignments__project=project)
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
-from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.urls import reverse
+from .forms import CustomUserCreationForm, TaskForm
+from .models import Project, Task
+from .mixins import ProjectRoleRequiredMixin
 
 def register_view(request):
     if request.method == 'POST':
@@ -8,8 +13,53 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('login') 
+            return redirect('dashboard') 
     else:
         form = CustomUserCreationForm()
-    
     return render(request, 'registration/register.html', {'form': form})
+
+class DashboardView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = 'projects/dashboard.html'
+    context_object_name = 'projects'
+
+    def get_queryset(self):
+        return Project.objects.filter(assignments__user=self.request.user)
+
+class ProjectDetailView(LoginRequiredMixin, ProjectRoleRequiredMixin, DetailView):
+    model = Project
+    template_name = 'projects/project_detail.html'
+    context_object_name = 'project'
+
+class TaskCreateView(LoginRequiredMixin, ProjectRoleRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'projects/task_form.html'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        project = get_object_or_404(Project, pk=self.kwargs['pk'])
+        kwargs['project'] = project
+        return kwargs
+
+    def form_valid(self, form):
+        project = get_object_or_404(Project, pk=self.kwargs['pk'])
+        form.instance.project = project
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('project_detail', kwargs={'pk': self.kwargs['pk']})
+
+class TaskUpdateView(LoginRequiredMixin, ProjectRoleRequiredMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'projects/task_form.html'
+    pk_url_kwarg = 'task_pk'
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['project'] = self.object.project
+        return kwargs
+
+    def get_success_url(self):
+        return reverse('project_detail', kwargs={'pk': self.object.project.pk})
